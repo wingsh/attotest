0b870be4a1b16c5b4054fca476739a22491121e2
Sunghwan Cho <sunghwan.cho@atto-research.com>
2016-12-21 11:23:23 +0900

    AU-157 login error case

--- src/test/java/attotest/variable.java
+++ src/test/java/attotest/variable.java
@@ -1,8 +1,11 @@
 package attotest;
 
 public class variable {
+	// ** Login Case **
+	// NULL UserID & Password 
 	String invaildNullInputErrMsg = "Input your id or password";
+	// Invaild UserID
 	String invalidUserErrMsg = "invalid password or id";
+	// Invaild Password
 	String invaldPasswordErrMsg = "invalid password or id";
-
 }
